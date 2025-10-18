d1be954b8b156d9f15e2d1803b7f80348cbc7e54
shraddhagreddy <shraddhagreddy@gmail.com>
2025-10-18 11:52:02 +0530

    adding else to try- lec4

--- lecture4/lec3.md
+++ lecture4/lec3.md
@@ -7,3 +7,14 @@
 ## try
 - can check whether something exceptional has happened 
 - ValueError: occurs when you give a function or operation a value of the right type but an inappropriate or invalid value.
+- while trying the try- except method, we should know the error right? 
+    like ValueError for example
+    what to do if u cannot anticipate the type of error?
+
+    ### ans
+    - u can omit ValueError and just say except
+    - but it is a bad practice as u wont know what is wrong with it
+    - try to figure out what is wrong 
+    - try with everything
+
+- we can use else as well
--- lecture4/number.py
+++ lecture4/number.py
@@ -19,8 +19,56 @@ not try and request them saying pls enter integer
 """
 
 #indentation is important
+
+"""
 try:
     x=int(input("what is x?"))
     print(f"x is {x}")
 except ValueError:
     print("x is not an integer")
+
+again this is not the best practice as 
+x=int(input("what is x?"))
+    print(f"x is {x}")
+
+is taking two lines so instead
+"""
+
+
+
+"""
+try:
+    x=int(input("what is x?"))
+except ValueError:
+    print("x is not an integer")
+print(f"x is {x}")
+
+
+now this error shows
+we are getting NameError
+
+what is x?cat
+x is not an integer
+Traceback (most recent call last):
+  File "/Users/shraddhagirishreddy/Desktop/harvard_python/lecture4/number.py", line 41, in <module>
+    print(f"x is {x}")
+                  ^
+NameError: name 'x' is not defined
+
+
+why is not defined eventhough we have tried to define it on line2
+- order of operations is wrong
+- input function is working
+- if passing that string to int function as argument
+- it is prolly int function is creating an error
+- the int(...) isnt working so no value is copied into x
+- how to solve?
+"""
+#else
+
+try:
+    x=int(input("what is x?"))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")
